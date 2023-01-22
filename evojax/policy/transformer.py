93000825ba98b8e1a121272e8b4739889dbff97e
Gautier Hamon <hamon.gautier.16@gmail.com>
2023-01-22 22:05:39 +0100

    small fix

--- evojax/policy/transformer.py
+++ evojax/policy/transformer.py
@@ -24,11 +24,11 @@ class PositionalEncoding(nn.Module):
 
     def setup(self):
         # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
-        pe = np.zeros((self.max_len, self.d_model))
-        position = np.arange(0, self.max_len, dtype=np.float32)[:, None]
-        div_term = np.exp(np.arange(0, self.d_model, 2) * (-math.log(10000.0) / self.d_model))
-        pe[:, 0::2] = np.sin(position * div_term)
-        pe[:, 1::2] = np.cos(position * div_term)
+        pe = jnp.zeros((self.max_len, self.d_model))
+        position = jnp.arange(0, self.max_len, dtype=jnp.float32)[:, None]
+        div_term = jnp.exp(jnp.arange(0, self.d_model, 2) * (-math.log(10000.0) / self.d_model))
+        pe[:, 0::2] = jnp.sin(position * div_term)
+        pe[:, 1::2] = jnp.cos(position * div_term)
         pe = pe[None]
         self.pe = jax.device_put(pe)
 
